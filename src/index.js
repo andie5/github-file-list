287252e96f0111140fae1a83dfd5a77b4be14bcc
andie5 <andieasa@googlemail.com>
2020-02-04 22:08:57 +0000

    added file list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,55 @@
 import React from "react";
+import PropTypes from "prop-types";
+import ReactDOM from "react-dom";
 
-const main = () => {};
+const FileList = ({ files }) => (
+  <table className="file-list">
+    <tbody>
+      {files.map(file => (
+        <tr className="file-list-item" key={file.id}>
+          {file.id}
+          <td className="file-name">{file.name}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
 
-ReactDOM.render(<App />, getElementById("root"));
+const testFiles = [
+  {
+    id: 1,
+    name: "src",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit"
+    }
+  },
+  {
+    id: 2,
+    name: "tests",
+    type: "folder",
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: "Initial commit"
+    }
+  },
+  {
+    id: 3,
+    name: "README",
+    type: "file",
+    updated_at: "2016-07-18 21:24:00",
+    latestCommit: {
+      message: "Added a readme"
+    }
+  }
+];
+
+FileList.propTypes = {
+  files: PropTypes.array
+};
+
+ReactDOM.render(
+  <FileList files={testFiles} />,
+  document.getElementById("root")
+);
